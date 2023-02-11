3bc6bb7455c25ede0186bcc76ca59e3005e42bf2
eliphatfs <flandre@scarletx.cn>
2023-02-11 21:00:29 +0800

    Add 3 steps of MNIST to test.

--- tests/test_tutorials.py
+++ tests/test_tutorials.py
@@ -1,13 +1,19 @@
 import os
 import sys
 import unittest
+import torchvision
+import torch_redstone as rst
 
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 
-# class TestTutorials(unittest.TestCase):
-#     def test_mnist(self):
-#         import tutorials.mnist
-#         tutorials.mnist.main(1, True)
-# Too slow for CPU CI machines
+class TestTutorials(unittest.TestCase):
+    def test_mnist(self):
+        from tutorials.mnist import MNISTTask
+        model = torchvision.models.resnet18(num_classes=10)
+        rst.DefaultLoop(
+            model, MNISTTask(), optimizer='adam',
+            adapter=rst.DirectPredictionAdapter(),
+            batch_size=32
+        ).run(1, max_steps=3, quiet=True)
